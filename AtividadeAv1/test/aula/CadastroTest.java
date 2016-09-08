77d88ef571894c44314e3772264d23f885f699c2
trgomes <trtg10@gmail.com>
2016-09-08 17:07:08 -0300

    senaTest finalizado

--- AtividadeAv1/test/aula/CadastroTest.java
+++ AtividadeAv1/test/aula/CadastroTest.java
@@ -2,13 +2,45 @@ package aula;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class CadastroTest {
+	
+	Cadastro cad;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	//Exercicio 2 - a
+	
+	@Test // "teste.txt"
+	public void construtor1() throws IOException{
+		cad = new Cadastro("teste.txt");
+		assertNotNull(cad);
 	}
 
+	@Test(expected=IOException.class) // ""
+	public void construtor2() throws IOException{
+		cad = new Cadastro("");
+		assertNotNull(cad);
+	}
+	
+	@Test(expected=NullPointerException.class)// null
+	public void construtor3() throws IOException{
+		cad = new Cadastro(null);
+		assertNotNull(cad);
+	}
+	
+	//Exercicio 2 - b
+	
+	@Test //1 e "De volta para o futuro I"
+	public void testInsert() throws IOException{
+		cad = new Cadastro("filmes.txt");		
+		cad .insert(1, "De volta para o futuro I");
+		
+		cad.imprimir();
+		
+//		assertEquals("1;De volta para o futuro I", cad.imprimir());	
+	}
+	
+	
 }
